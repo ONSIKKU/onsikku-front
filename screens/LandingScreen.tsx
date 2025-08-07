4b9cfd33a465a5faa1109450d5bab568551e3dcb
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-08 02:23:19 +0900

    feat: 랜딩 페이지 초기 UI 제작 (#2)

--- screens/LandingScreen.tsx
+++ screens/LandingScreen.tsx
@@ -2,8 +2,18 @@ import { Text, View } from "react-native";
 
 export default function LandingScreen() {
   return (
-    <View>
-      <Text>랜딩 페이지</Text>
+    <View className="bg-background-orange flex-1 justify-center items-center">
+      <Text className="text-7xl text-black font-bold">온식구</Text>
+      <Text className="text-2xl text-black font-semibold">
+        매일 5분, 가족과 더 가까워지는 시간
+      </Text>
+      <Text className="text-2xl text-black font-bold">카카오톡 넣을거</Text>
+      <Text className="text-2xl text-black font-bold">
+        카카오톡 계정으로 간편하게
+      </Text>
+      <Text className="text-2xl text-black font-bold">
+        가족과의 소중한 시간을 시작해보세요
+      </Text>
     </View>
   );
 }
