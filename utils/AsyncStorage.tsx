7f744801e4a5b4af3152c89990f66d42c6c4a73f
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-21 17:48:22 +0900

    feat: Async Storage 함수 정의 (#19)

---
+++ utils/AsyncStorage.tsx
@@ -0,0 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const setItem = async (key: string, value: string) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+
+    // 저장값 확인을 위한 console.log
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const getItem = async (key: string) => {
+  try {
+    const res = await AsyncStorage.getItem(key);
+    return res || "";
+  } catch (e) {
+    throw e;
+  }
+};
