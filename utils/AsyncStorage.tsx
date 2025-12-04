0d522338ca00812402916102ff93e641e6b39af6
jebiyeon02 <jhje5595@naver.com>
2025-12-04 19:35:11 +0900

    chore: 주석 제거

--- utils/AsyncStorage.tsx
+++ utils/AsyncStorage.tsx
@@ -3,8 +3,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const setItem = async (key: string, value: string) => {
   try {
     await AsyncStorage.setItem(key, value);
-
-    // 저장값 확인을 위한 console.log
   } catch (e) {
     throw e;
   }
